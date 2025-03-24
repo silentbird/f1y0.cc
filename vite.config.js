07449a4554c1e6dca64d43bfa840bc6318e8cd47
silentbird <silentbird520@outlook.com>
2025-03-25 00:35:58 +0800

    修改路径报错

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,22 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-vendor': ['vue', 'vue-router', 'pinia'],
+        },
+      },
     },
   },
   server: {
